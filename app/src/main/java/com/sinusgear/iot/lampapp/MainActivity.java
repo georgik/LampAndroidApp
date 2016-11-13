ea3359c0d6ada4c28c9420617b2ba8987cd2e062
Juraj Michalek <juraj.michalek@ysoft.com>
2016-11-13 10:46:27 +0100

    check and enable wifi before sending request

--- app/src/main/java/com/sinusgear/iot/lampapp/MainActivity.java
+++ app/src/main/java/com/sinusgear/iot/lampapp/MainActivity.java
@@ -1,5 +1,9 @@
 package com.sinusgear.iot.lampapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,8 +17,30 @@ import cz.msebera.android.httpclient.Header;
 public class MainActivity extends AppCompatActivity {
 
     protected boolean isRelayEnabled = false;
+    protected boolean lastWifiState = false;
 
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void changeWifiStatus(boolean status) {
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(status);
+    }
+
+    /**
+     * Send HTTP request. When Wifi is down it will turn it on and then off.
+     * @param state
+     */
     private void getHttpRequest(String state) {
+        lastWifiState = isNetworkAvailable();
+        if (!lastWifiState) {
+            changeWifiStatus(true);
+        }
+
         AsyncHttpClient asyncClient = new AsyncHttpClient();
         asyncClient.get("http://192.168.1.50/relay=" + state, new AsyncHttpResponseHandler() {
             @Override
@@ -23,12 +49,16 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
+                if (!lastWifiState) {
+                    changeWifiStatus(false);
+                }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
+                if (!lastWifiState) {
+                    changeWifiStatus(false);
+                }
             }
 
         });
